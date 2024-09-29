21ffd55ba8d5d3462d53463a1c1fa4f263a5b1c0
Munir <smsirajulmonir@gmail.com>
2024-09-29 11:17:28 -0700

    make user to admin success

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,15 +1,39 @@
-import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaBook, FaCalendar, FaEnvelope, FaHome, FaList, FaSearch, FaShoppingCart, FaUser, FaUsers, FaUtensils, FaVoicemail } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
     const [cart] = useCart();
+
+    // Todo: get admin value from the database
+    const isAdmin = true;
+
     return (
         <div className="flex">
             {/* left side */}
            <div className="w-64 min-h-screen bg-orange-100">
                 <ul className="menu p-4">
+                   {
+                    isAdmin ? <>
+                     <li>
+                        <NavLink to="/dashboard/adminHome"><FaHome></FaHome>Admin Home</NavLink>
+                    </li>
                     <li>
+                        <NavLink to="/dashboard/addProducts"><FaUtensils></FaUtensils>Add Products</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/mangeProducts"><FaList></FaList>Manage Products</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/bookings"><FaBook></FaBook>Manage Bookings</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/users"><FaUsers></FaUsers>All Users</NavLink>
+                    </li>
+                    </>
+                    :
+                    <>
+                     <li>
                         <NavLink to="/dashboard/userHome"><FaHome></FaHome>User Home</NavLink>
                     </li>
                     <li>
@@ -24,12 +48,18 @@ const Dashboard = () => {
                     <li>
                         <NavLink to="/dashboard/review"><FaList></FaList>My Booking</NavLink>
                     </li>
+                    </>
+                   }
+                    {/* shared nav links */}
                     <div className="divider"></div>
                     <li>
                         <NavLink to="/"><FaHome></FaHome>Home</NavLink>
                     </li>
                     <li>
                         <NavLink to="/order/Dell"><FaSearch></FaSearch>Products</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/order/contact"><FaEnvelope></FaEnvelope>Contact</NavLink>
                     </li>
                 </ul>
             </div> 
